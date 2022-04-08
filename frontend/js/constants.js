b3dc9fb6fcc8f48ef03629c3a807662995cc67ef
NFTGENRAtOR <abdoudark2019@gmail.com>
2022-04-08 16:17:41 +0000

    switched to polygon

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,5 +1,5 @@
-const contractAddress = "0x2c7bB710859EAb2Db8FAB19d8D09fC985c320de3"; //Replace with your own contract address
-const chain = 'rinkeby'; // rinkeby or polygon
+const contractAddress = "0x32c8f94379Fd18f6b32989854E6d848BA502bcfa"; //Replace with your own contract address
+const chain = 'polygon'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CoolPunks NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
